2d120c0a3690666f2d471e91d717d7a85b11b6c2
Ojiteli Kosisochukwu <obuekwekosi@gmail.com>
2025-01-23 14:44:25 +0100

    fix

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,6 +8,7 @@ import todo from "../images/todolist.png";
 import vivys from "../images/vivysfashion.png";
 import elva from "../images/elva.png";
 import famtrust from "../images/famtrustproject.png";
+import laundry from "../images/laundyEase.png"
 // import safe from "../images/safe.png";
 import safe from "../images/mini.png";
 
@@ -27,6 +28,34 @@ const Projects = () => {
             <h4 className="my">MY PROJECTS</h4>
           </div>
           <div className="project">
+          <div
+              data-aos="flip-down"
+              data-aos-delay="400"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="600"
+            >
+              <div>
+                <img src={laundry} alt="" />
+              </div>
+              <div className="agroweb">
+                <h2>LAUNDRY EASE</h2>
+                <div>
+                  <a
+                    href="https://laundry-ease.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="icon">
+                      <SiVercel size={30} />
+                    </div>
+                  </a>
+                </div>
+              </div>
+
+              <p>
+              The laundry website allows users to schedule and manage their laundry services online. It provides features like service selection, pickup and delivery scheduling, pricing details, and real-time order tracking to streamline and simplify the laundry process for customers
+              </p>
+            </div>
             <div
               data-aos="flip-down"
               data-aos-delay="400"
@@ -34,7 +63,7 @@ const Projects = () => {
               data-aos-duration="600"
             >
               <div>
-                <img src={agro} alt="" />
+                <img src={agro} alt="agro" />
               </div>
               <div className="agroweb">
                 <h2>AGRO WEBSITE</h2>
@@ -70,7 +99,7 @@ const Projects = () => {
                 <h2>FARMTRUST</h2>
                 <div>
                   <a
-                    href="famtrustproject.vercel.app"
+                    href="http://famtrustproject.vercel.app/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
@@ -127,7 +156,7 @@ const Projects = () => {
                 <h2>PROJECT-ELVA</h2>
                 <div>
                   <a
-                    href="https://elva-project-9ibi.vercel.app/"
+                    href="https://elva-project.vercel.app/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
